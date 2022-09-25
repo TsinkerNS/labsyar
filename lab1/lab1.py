59d639fe1119d975c125894d7efb341c283bc916
Hutsker <haker.wf@yandex.ru>
2022-09-25 18:07:19 +0500

    frirst var #

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,1 +1,51 @@
-print('lab 1') 
+# Список
+#list1 = ['Дом', 'Подъезд']
+#print(list1[1])
+#
+#list1.append('Кампус')
+#print(list1)
+#
+#list2 = ['Облако', 'Сетевой объект']
+#print(list1 + list2)
+#
+#list2.insert(0, 'Представление')
+#print(list2)
+
+# Кортеж
+#company = 'Акадо', 'Энфорта', 'Инфосети'
+#print(company)
+#
+#companys = (['ГУТС', 'Энфорта'], ['Акадо', 'Инфосети'])
+#print(companys[1])
+#
+#companys[1].append('Авантел')
+#print(companys[1])
+
+# Множества
+#class_dev = ['Tp-Link', 'D-Link', 'Cisco']
+#class_dev_set = set()
+#print(class_dev_set)
+#
+#class_dev_set.update(class_dev)
+#print(class_dev_set)
+#
+#class_dev_set.add('Huawei')
+#print(class_dev_set)
+#
+#class_dev_set.remove('Cisco')
+#print(class_dev_set)
+
+
+# Словари
+
+#fiature = {
+#    'Признаки подключения ФЛ' : 'Дом подключен(ТВ)',
+#    'Признаки подключения ФЛ' : 'Дом подключен(Инетрнет)',
+#    'Признаки подключения ФЛ' : 'Дом подключен(IPTV)',
+#    'Признаки подключения ЮЛ' : 'В доме есть ЮЛ',
+#    'Признаки подключения ЮЛ' : 'Только ЮЛ(сх. 2, 3)',
+#    'Признаки подключения ЮЛ' : 'Только ЮЛ(сх. 1)'
+#}
+#print(len(fiature))
+#print('Признаки подключения ФЛ' in fiature)
+#print('Только ЮЛ(сх. 1)' in fiature.values()) 
